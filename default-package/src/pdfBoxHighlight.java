45e9dabcfcff4581892d5b0b38b71aa0f11678ef
treguv <vladislavtregubov00@gmail.com>
2020-10-17 15:21:17 -0700

    Rewrote pdfBoxHighlight into ParseHighlights

--- default-package/src/pdfBoxHighlight.java
+++ default-package/src/pdfBoxHighlight.java
@@ -13,71 +13,9 @@ import java.util.List;
 public class pdfBoxHighlight {
 
     public static void main(String[]args) throws IOException {
-        ArrayList<String> testGetHighlights = new ArrayList<>();
-        testGetHighlights = getHighlightedText("C:\\Users\\Public\\Documents\\first_pdf.pdf", 0);
-        System.out.println(testGetHighlights.toString());
+        ParseHighlights highlights = new ParseHighlights("C:\\Users\\vladi\\Downloads\\New folder\\test-0.pdf");
+        System.out.println(highlights.toString());
     }
 
-    public static ArrayList<String> getHighlightedText(String filePath, int pageNumber) throws IOException {
-        ArrayList<String> highlightedTexts = new ArrayList<>();
-        // this is the in-memory representation of the PDF document.
-        // this will load a document from a file.
 
-        PDDocument document = PDDocument.load(new File(filePath));
-        // this represents all pages in a PDF document.
-        List<PDPage> allPages = new ArrayList<>();
-        document.getDocumentCatalog().getPages().forEach(allPages::add);
-        // this represents a single page in a PDF document.
-        PDPage page = allPages.get(pageNumber);
-        // get  annotation dictionaries
-        List<PDAnnotation> annotations = page.getAnnotations();
-
-        for(int i=0; i<annotations.size(); i++) {
-            // check subType
-            if(annotations.get(i).getSubtype().equals("Highlight")) {
-                // extract highlighted text
-                PDFTextStripperByArea stripperByArea = new PDFTextStripperByArea();
-
-                COSArray quadsArray = (COSArray) annotations.get(i).getCOSObject().getDictionaryObject(COSName.getPDFName("QuadPoints"));
-                String str = null;
-
-                for(int j=1, k=0; j<=(quadsArray.size()/8); j++) {
-
-                    COSFloat ULX = (COSFloat) quadsArray.get(0+k);
-                    COSFloat ULY = (COSFloat) quadsArray.get(1+k);
-                    COSFloat URX = (COSFloat) quadsArray.get(2+k);
-                    COSFloat URY = (COSFloat) quadsArray.get(3+k);
-                    COSFloat LLX = (COSFloat) quadsArray.get(4+k);
-                    COSFloat LLY = (COSFloat) quadsArray.get(5+k);
-                    COSFloat LRX = (COSFloat) quadsArray.get(6+k);
-                    COSFloat LRY = (COSFloat) quadsArray.get(7+k);
-
-                    k+=8;
-
-                    float ulx = ULX.floatValue() - 1;                           // upper left x.
-                    float uly = ULY.floatValue();                               // upper left y.
-                    float width = URX.floatValue() - LLX.floatValue();          // calculated by upperRightX - lowerLeftX.
-                    float height = URY.floatValue() - LLY.floatValue();         // calculated by upperRightY - lowerLeftY.
-
-                    PDRectangle pageSize = page.getMediaBox();
-                    uly = pageSize.getHeight() - uly;
-
-                    Rectangle2D.Float rectangle_2 = new Rectangle2D.Float(ulx, uly, width, height);
-                    stripperByArea.addRegion("highlightedRegion", rectangle_2);
-                    stripperByArea.extractRegions(page);
-                    String highlightedText = stripperByArea.getTextForRegion("highlightedRegion");
-
-                    if(j > 1) {
-                        str = str.concat(highlightedText);
-                    } else {
-                        str = highlightedText;
-                    }
-                }
-                highlightedTexts.add(str);
-            }
-        }
-        document.close();
-
-        return highlightedTexts;
-    }
 }
