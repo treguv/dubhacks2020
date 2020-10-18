102c2b699ead1742fe3670db00a8540885a7cc7a
yavuzalp <turkoglu.yavuzalp@gmail.com>
2020-10-17 17:04:13 -0700

    Changes on Highlight class

--- default-package/src/Highlight.java
+++ default-package/src/Highlight.java
@@ -1,60 +1,34 @@
-import java.util.ArrayList;
 
 class Highlight{
-    protected int myLocationX;
-    protected int myLocationY;
-    protected int myLocationWidth;
-    protected int myLocationHeight;
-    protected int myAmount;
-    protected ArrayList<String> myStudents;
+    protected String myText;
+    protected int myPage;
+    protected int myCount;
 
-    public Highlight(int locationX, int locationY, int locationWidth, int locationHeight, int amount){
-        myLocationX = locationX;
-        myLocationY = locationY;
-        myLocationWidth = locationWidth;
-        myLocationHeight = locationHeight;
-        myAmount = amount;
-        myStudents = new ArrayList<>();
+    public Highlight(String theText, int thePage, int theCount){
+        myText = theText;
+        myPage = thePage;
+        myCount = theCount;
     }
 
-    public Highlight(int locationX, int locationY, int locationWidth, int locationHeight){
-        myLocationX = locationX;
-        myLocationY = locationY;
-        myLocationWidth = locationWidth;
-        myLocationHeight = locationHeight;
-        myAmount = 1;
-        myStudents = new ArrayList<>();
+    public Highlight(String theText, int thePage){
+        myText = theText;
+        myPage = thePage;
+        myCount = 1;
     }
 
-    public void addStudent(String name){
-        myStudents.add(name);
+    public Boolean compare(Highlight theOtherHighlight){
+        return myText.equals(theOtherHighlight.getText());
     }
 
-    public Boolean compare(Highlight otherHighlight){
-        Boolean isEqual = false;
-        //TO DO: compare two highlight objects and see if they overlap.
-        return isEqual;
+    public void increaseCount(){
+        myCount++;
     }
 
-    public void increaseAmount(){
-        myAmount++;
+    public int getCount() {
+        return myCount;
     }
 
-    public void decreaseAmount(){
-        myAmount--;
+    public String getText() {
+        return myText;
     }
-
-    public int getAmount() {
-        return myAmount;
-    }
-
-    public ArrayList<String> getStudents() {
-        return myStudents;
-    }
-
-    public int[] getMyLocation() {
-        int[] location = new int[]{myLocationX, myLocationY, myLocationWidth, myLocationHeight};
-        return location;
-    }
-
 }
